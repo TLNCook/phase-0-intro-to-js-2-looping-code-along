9e5997eb6c4e965b64f2b8029c314bc8d7b00090
Thomas Cook <tlncook85@gmail.com>
2022-05-17 23:18:06 -0600

    commit tree ad2b91d24eaccac50ae3726c769203430d852d1b

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(nameArr, adjective) {
+    const newArr = [];
+    for (let name of nameArr) {
+        newArr.push(`Thank you, ${name}, for the wonderful ${adjective} gift!`)
+    }
+    return newArr;
+}
+function countDown(number) {
+    for(let i = number; i >= 0; i--) {
+        console.log(number)
+        number --;
+    }
+}
